c0518b57a2ddedd9f607e5fa1c7901198b6fed7b
1-Akshay <akshayshetty.mum.dbda@gmail.com>
2023-12-05 17:44:23 +0530

    resoloved some import errors and formatted code according to PEP 8

--- lstm_new.py
+++ lstm_new.py
@@ -8,11 +8,11 @@ Original file is located at
 """
 
 import yfinance
-import pandas as pd, numpy as np
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
+import keras
+from keras import layers
 from sklearn.preprocessing import MinMaxScaler
 
 start = '2000-01-01'
@@ -32,7 +32,7 @@ plt.ylabel('Prices ($)')
 data_training = pd.DataFrame(df['Close'][0:int(len(df)*0.80)])
 data_testing = pd.DataFrame(df['Close'][int(len(df)*0.80):int(len(df))])
 
-scaler = MinMaxScaler(feature_range=(0,1))
+scaler = MinMaxScaler(feature_range=(0, 1))
 data_training_array = scaler.fit_transform(data_training)
 
 x_train = []
@@ -40,23 +40,23 @@ y_train = []
 
 for i in range(60, data_training_array.shape[0]):
     x_train.append(data_training_array[i-60:i])
-    y_train.append(data_training_array[i,0])
+    y_train.append(data_training_array[i, 0])
 
-x_train , y_train = np.array(x_train) , np.array(y_train)
+x_train, y_train = np.array(x_train), np.array(y_train)
 
 past_60_days = data_training.tail(60)
-final_df= past_60_days.append(data_testing,ignore_index=True)
+final_df = past_60_days.append(data_testing, ignore_index=True)
 
-input_data= scaler.fit_transform(final_df)
+input_data = scaler.fit_transform(final_df)
 
-x_test=[]
-y_test=[]
+x_test = []
+y_test = []
 
-for i in range(60,input_data.shape[0]):
+for i in range(60, input_data.shape[0]):
     x_test.append(input_data[i-60:i])
-    y_test.append(input_data[i,0])
+    y_test.append(input_data[i, 0])
 
-x_test , y_test = np.array(x_test) , np.array(y_test)
+x_test, y_test = np.array(x_test), np.array(y_test)
 
 model = keras.Sequential()
 model.add(layers.LSTM(100, return_sequences=True, input_shape=(x_train.shape[1], 1)))
@@ -66,18 +66,18 @@ model.add(layers.Dense(1))
 model.summary()
 
 model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(x_train, y_train, batch_size= 1, epochs=3)
+model.fit(x_train, y_train, batch_size=1, epochs=3)
 
 y_predicted = model.predict(x_test)
 
-y_predicted=scaler.inverse_transform(y_predicted)
-y_test=y_test.reshape(-1,1)
-y_test=scaler.inverse_transform(y_test)
+y_predicted = scaler.inverse_transform(y_predicted)
+y_test = y_test.reshape(-1, 1)
+y_test = scaler.inverse_transform(y_test)
 
-fig2 = plt.figure(figsize=(12,8))
+fig2 = plt.figure(figsize=(12, 8))
 # plt.plot(ff,'b',label='CLosing')
-plt.plot(y_test , 'r', label='OG price')
-plt.plot(y_predicted , 'b', label='Predicted price')
+plt.plot(y_test, 'r', label='OG price')
+plt.plot(y_predicted, 'b', label='Predicted price')
 plt.xlabel('Time')
 plt.ylabel('Price')
 plt.legend()
@@ -99,54 +99,53 @@ plt.legend()
 
 # pred_price=scaler.inverse_transform(pred_price)
 
-n_steps=60
-pred_days=30
-x_input=input_data[len(input_data)-n_steps:].reshape(1,-1)
-x_input.shape
-temp_input=list(x_input)
-temp_input=temp_input[0].tolist()
+n_steps = 60
+pred_days = 30
+x_input = input_data[len(input_data)-n_steps:].reshape(1, -1)
+# x_input.shape
+temp_input = list(x_input)
+temp_input = temp_input[0].tolist()
 
 # demonstrate prediction for next N days
-from numpy import array
+# from numpy import array
 
-lst_output=[]
+lst_output = []
 
-i=0
-while(i<pred_days):
+i = 0
+while i < pred_days:
 
-    if(len(temp_input)>60):
-        #print(temp_input)
-        x_input=np.array(temp_input[1:])
+    if len(temp_input) > 60:
+        # print(temp_input)
+        x_input = np.array(temp_input[1:])
         # print("{} day input {}".format(i,x_input))
-        x_input=x_input.reshape(1,-1)
+        x_input = x_input.reshape(1, -1)
         x_input = x_input.reshape((1, n_steps, 1))
-        #print(x_input)
+        # print(x_input)
         yhat = model.predict(x_input, verbose=0)
         # print("{} day output {}".format(i,yhat))
         temp_input.extend(yhat[0].tolist())
-        temp_input=temp_input[1:]
-        #print(temp_input)
+        temp_input = temp_input[1:]
+        # print(temp_input)
         lst_output.extend(yhat.tolist())
-        i=i+1
+        i = i+1
     else:
-        x_input = x_input.reshape((1, n_steps,1))
+        x_input = x_input.reshape((1, n_steps, 1))
         yhat = model.predict(x_input, verbose=0)
         # print(yhat[0])
         temp_input.extend(yhat[0].tolist())
         # print(len(temp_input))
         lst_output.extend(yhat.tolist())
-        i=i+1
+        i = i+1
 
-predictions=scaler.inverse_transform(lst_output)
+predictions = scaler.inverse_transform(lst_output)
 
-day_new=np.arange(1,61)
-day_pred=np.arange(61,61+pred_days)
+day_new = np.arange(1, 61)
+day_pred = np.arange(61, 61+pred_days)
 
-fig2 = plt.figure(figsize=(12,8))
-#plt.plot(ff,'b',label='CLosing')
-plt.plot(day_new,(df.Close[len(df)-60:]) , 'g', label='OG price')
-plt.plot(day_pred,scaler.inverse_transform(lst_output) , 'r', label='Predicted price')
+fig2 = plt.figure(figsize=(12, 8))
+# plt.plot(ff,'b',label='CLosing')
+plt.plot(day_new, (df.Close[len(df)-60:]), 'g', label='OG price')
+plt.plot(day_pred, scaler.inverse_transform(lst_output), 'r', label='Predicted price')
 plt.xlabel('Time')
 plt.ylabel('Price')
 plt.legend()
-
